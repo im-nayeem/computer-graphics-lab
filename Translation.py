88e97f5ead0eae79c0bd8ddf0b6252e9c7dfe0f5
Nayeem Hossain <nayeem.cse.cou@gmail.com>
2023-06-13 14:42:16 +0600

    update Translation.py

--- Translation.py
+++ Translation.py
@@ -10,19 +10,23 @@ def translate(drawer, coordinates, tx, ty):
 
      
 def main():
-
+    # List containing the coordinates
     coordinates = [{'x':1, 'y':4}, {'x':2, 'y':4}, 
                    {'x':3, 'y':4}, {'x':4, 'y':4},
                    {'x':5, 'y':4}, {'x':4, 'y':5},
                    {'x':2, 'y':5}, {'x':3, 'y':5}, {'x':3, 'y':6}]
-
+    
+    # translation vector [tx, ty]
     tx, ty = 5, 8 
 
     drawer = pixeldrawer.PixelDrawer()
     drawer.start()
+
+    # Before Translation
     for item in coordinates:
         drawer.draw_pixel(item['x'], item['y'])
 
+    # After Translation
     translate(drawer, coordinates, tx, ty)
     
     drawer.execute()
